e261dbde982557797c9625af4eb84af7c3a41ddc
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-04-11 21:09:08 -0300

    iniciando a criação do vetor

---
+++ estruturas/Estruturas/Estruturas/Vetor/Vetor.cs
@@ -0,0 +1,23 @@
+﻿namespace Estruturas.Vetor;
+
+public class Vetor
+{
+    private string[] Elementos;
+
+    public Vetor(int capacidade)
+    {
+        this.Elementos = new string[capacidade];
+    }
+
+    public void adiciona(string elemento)
+    {
+        for (int i = 0; i < this.Elementos.Length; i++)
+        {
+            if (Elementos[i] == null)
+            {
+                Elementos[i] = elemento;
+                break;
+            }
+        }
+    }
+}
